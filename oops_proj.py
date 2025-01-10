5a52307a06ae2289983cc77535ecf871324f573a
Akshay <akshayparate61@gmail.com>
2025-01-10 18:33:18 +0530

    project - basic structure ready

---
+++ oops_proj.py
@@ -0,0 +1,38 @@
+class chatbook:
+    def __init__(self):
+        self.username = ''
+        self.password = ''
+        self.loggedin = False
+        self.menu()
+
+    def menu(self):
+        user_input = input("""Welcome to chatbook !! How would you like to press"
+                           1. Press 1 to signup
+                           2. Press 2 to signin
+                           3. Press 3 to write a post
+                           4. Press 4 to message a friend
+                           5. Press any other key to exit""")
+        if user_input == "1":
+            self.signup()
+        elif user_input == "2":
+            pass
+        elif user_input == "3":
+            pass
+        elif user_input == "4":
+            pass
+        else:
+            exit()
+    
+    def signup(self):
+        email = input("enter your email here -> ")
+        pwd = input("setup your password here -> ")
+        self.username = email
+        self.password = pwd
+        print("you have signed up successfully !!")
+        print("\n")
+        self.menu()
+
+obj = chatbook()        
+
+        
+        
